e364caf2764f86c78b411f7b91f7032df43fcce9
Volodymyr web2026-1 <volodymyrnakladiuk@gmail.com>
2026-05-07 16:14:47 +0300

    cash

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const fs = require('node:fs');
+const path = require('node:path');
 const http = require('node:http');
 const { program } = require('commander');
 
@@ -26,10 +27,55 @@ console.log(`Cache directory: ${options.cache}`);
 
 const host = options.host;
 const port = Number(options.port);
+const cacheDir = options.cache;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Server is running');
+function getStatusCodeFromUrl(url) {
+  return url.replace(/^\/+/, '').split('/')[0] || '';
+}
+
+function getCacheFilePath(code) {
+  return path.join(cacheDir, `${code}.jpg`);
+}
+
+function isValidCode(code) {
+  return /^\d+$/.test(code);
+}
+
+function sendText(res, statusCode, text) {
+  res.writeHead(statusCode, { 'Content-Type': 'text/plain' });
+  res.end(text);
+}
+
+function sendImage(res, imageBuffer, statusCode = 200) {
+  res.writeHead(statusCode, { 'Content-Type': 'image/jpeg' });
+  res.end(imageBuffer);
+}
+
+const server = http.createServer(async (req, res) => {
+  const code = getStatusCodeFromUrl(req.url);
+
+  if (!code || !isValidCode(code)) {
+    sendText(res, 400, 'Bad Request');
+    return;
+  }
+
+  if (req.method !== 'GET') {
+    sendText(res, 405, 'Method Not Allowed');
+    return;
+  }
+
+  const filePath = getCacheFilePath(code);
+
+  try {
+    const image = await fs.promises.readFile(filePath);
+    sendImage(res, image, 200);
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      sendText(res, 404, 'Not Found');
+      return;
+    }
+    sendText(res, 500, 'Internal Server Error');
+  }
 });
 
 server.listen(port, host, () => {
